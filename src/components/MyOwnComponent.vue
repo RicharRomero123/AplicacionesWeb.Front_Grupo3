060c9555d0e687000d16ae0e231a9d3d378b7dcd
Richar Romero <u20201b428@upc.edu.pe>
2023-04-04 21:04:18 -0500

    Initial commit

---
+++ src/components/MyOwnComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyOwnComponent",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="grid-container">
+    <div class="grid-item item-1">
+      <MyOwnComponent title="Título del componente 1" description="Descripción del componente 1" />
+    </div>
+    <div class="grid-item item-2">
+      <MyOwnComponent title="Título del componente 2" description="Descripción del componente 2" />
+    </div>
+    <div class="grid-item item-3">
+      <MyOwnComponent title="Título del componente 3" description="Descripción del componente 3" />
+    </div>
+  </div>
+</template>
+
+<script>
+import MyOwnComponent from "@/components/MyOwnComponent.vue";
+
+export default {
+  name: "GridPage",
+  components: {
+    MyOwnComponent,
+  },
+};
+</script>
+
+<style>
+.grid-container {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 20px;
+  margin: 20px;
+}
+
+.grid-item {
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  transition: all 0.3s ease;
+}
+
+.item-1 {
+  grid-column: 1 / span 2;
+}
+
+.item-2 {
+  grid-column: 3;
+  grid-row: 1 / span 2;
+}
+
+.item-3 {
+  grid-column: 1;
+  grid-row: 2 / span 2;
+}
+</style>
