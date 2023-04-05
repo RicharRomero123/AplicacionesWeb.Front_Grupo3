6cca5e7d1acbb35b4e479557bf71d86393b0bd17
FabrizCastro <fabrizziocastro2612@gmail.com>
2023-04-04 22:54:08 -0500

    Create footer.vue

---
+++ src/components/footer.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container-footer">
+         <div class="footer">
+            <div class="copyright">
+                © 2022 Todos los Derechos Reservados | <a href="">Redcorp</a>
+            </div>
+
+            <div class="information">
+                <a href="">Informacion Compañia</a> | <a href="">Privacion y Politica</a> | <a href="">Terminos y Condiciones</a>
+            </div>
+                
+            <div class="col-md-3 mb-2">
+                <h6 class="text-uppercase font-weight-bold">
+                  <label>Idioma</label>
+                  <select name="idioma" id="idioma">
+                    <option value="Español">Español</option>
+                    <option value="Inglés">Inglés</option>
+                  </select>
+                </h6>
+            </div>
+
+         </div>
+    </div>
+
+</template>
+
+<style>
+
+</style>
