bf3e9af3a7f4acbae5b5e50afd1f071a031139af
Richar Romero <u20201b428@upc.edu.pe>
2023-04-04 21:32:43 -0500

    v1

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,55 +1,144 @@
 <template>
-  <div class="grid-container">
-    <div class="grid-item item-1">
-      <MyOwnComponent title="Título del componente 1" description="Descripción del componente 1" />
+  <header class="header">
+    <div class="menu">
+      <button class="menu-button" @click="toggleMenu">
+        <span class="menu-icon"></span>
+        <span class="menu-icon"></span>
+        <span class="menu-icon"></span>
+      </button>
+      <nav class="menu-items" :class="{ 'show-menu': isMenuOpen }">
+        <ul>
+          <li><a href="#">Inicio</a></li>
+          <li><a href="#">Categorías</a></li>
+          <li><a href="#">Sobre nosotros</a></li>
+          <li><a href="#">Contáctanos</a></li>
+        </ul>
+      </nav>
     </div>
-    <div class="grid-item item-2">
-      <MyOwnComponent title="Título del componente 2" description="Descripción del componente 2" />
+    <div class="logo">LIBRARY UPC</div>
+    <div class="login">
+      <form>
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username">
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password">
+        <button type="submit">Log In</button>
+      </form>
     </div>
-    <div class="grid-item item-3">
-      <MyOwnComponent title="Título del componente 3" description="Descripción del componente 3" />
-    </div>
-  </div>
+  </header>
 </template>
 
-<script>
-import MyOwnComponent from "@/components/MyOwnComponent.vue";
-
-export default {
-  name: "GridPage",
-  components: {
-    MyOwnComponent,
-  },
-};
-</script>
-
 <style>
-.grid-container {
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-gap: 20px;
-  margin: 20px;
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: black;
+  color: white;
+  padding: 10px;
 }
 
-.grid-item {
-  background-color: #fff;
-  padding: 20px;
-  border-radius: 10px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  transition: all 0.3s ease;
+.menu {
+  display: flex;
+  align-items: center;
 }
 
-.item-1 {
-  grid-column: 1 / span 2;
+.menu-button {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 30px;
+  height: 20px;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  padding: 0;
+  margin: 0;
 }
 
-.item-2 {
-  grid-column: 3;
-  grid-row: 1 / span 2;
+.menu-icon {
+  width: 100%;
+  height: 3px;
+  background-color: white;
+  border-radius: 5px;
 }
 
-.item-3 {
-  grid-column: 1;
-  grid-row: 2 / span 2;
+.menu-items {
+  position: absolute;
+  top: 50px;
+  right: 0;
+  background-color: black;
+  color: white;
+  width: 200px;
+  height: 0;
+  overflow: hidden;
+  transition: height 0.3s ease;
+}
+
+.show-menu {
+  height: 120px;
+}
+
+.menu-items ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.menu-items li {
+  padding: 10px;
+}
+
+.menu-items a {
+  color: white;
+  text-decoration: none;
+}
+
+.logo {
+  font-size: 28px;
+}
+
+.login {
+  display: flex;
+  align-items: center;
+}
+
+form {
+  display: flex;
+  align-items: center;
+  margin: 0;
+}
+
+label {
+  margin-right: 10px;
+}
+
+input {
+  padding: 5px;
+  margin-right: 10px;
+}
+
+button {
+  padding: 5px 10px;
+  background-color: white;
+  color: black;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
 }
 </style>
+
+<script>
+export default {
+  data() {
+    return {
+      isMenuOpen: false,
+    }
+  },
+  methods: {
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen;
+    }
+  }
+}
+</script>
