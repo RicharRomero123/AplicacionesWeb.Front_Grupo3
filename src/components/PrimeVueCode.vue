36f3fae5b643e9c5b9a71bc708aaf659bce640fe
Richar Romero <u20201b428@upc.edu.pe>
2023-04-12 15:21:01 -0500

    create button

--- src/components/PrimeVueCode.vue
+++ src/components/PrimeVueCode.vue
@@ -1,19 +1,25 @@
 <template>
   <div class="input-wrapper">
     <span>Code:</span>
-    <pv-inputmask id="basic" v-model="value" mask="99-999999" placeholder="99-999999" />
+    <pv-inputmask id="basic" v-model="value" mask="99-999999" placeholder="99-999999" class="input-mask" />
+    <pv-button class="my-button" label="Validate" />
   </div>
 </template>
 
 <script>
+import Button from "primevue/button";
+
 export default {
-  name: "PrimeVueCode.vue",
+  name: "PrimeVueCode",
   data() {
     return {
-      value: ""
+      value: "",
     };
-  }
-}
+  },
+  components: {
+    Button,
+  },
+};
 </script>
 
 <style scoped>
@@ -24,5 +30,22 @@ export default {
 .input-wrapper span {
   margin-right: 10px;
 }
+.input-mask {
+  margin-right: 10px;
+}
+.my-button {
+  font-size: 1.2em;
+  padding: 10px 20px;
+  margin-left: 10px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+.my-button:hover {
+  background-color: #0062cc;
+}
 </style>
+
 
