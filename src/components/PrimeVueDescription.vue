df81e32cfaee2c371e6c14678bd88a0730d04dff
Richar Romero <u20201b428@upc.edu.pe>
2023-04-11 20:40:36 -0500

    Create components

---
+++ src/components/PrimeVueDescription.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "PrimeVueDescription.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/PrimeVueName.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "PrimeVueName.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
