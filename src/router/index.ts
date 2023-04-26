e5954dda8366e374c29ac332d29b26f9393f417d
RicharRomero123 <89022073+RicharRomero123@users.noreply.github.com>
2023-04-25 21:59:51 -0500

    Routing home /users v1

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import userspage from "../components/userspage.vue";
+import usersPage from "@/components/users-page.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -12,7 +12,7 @@ const router = createRouter({
     {
       path: '/userspage',
       name: 'userspage',
-      component: userspage
+      component: usersPage
     },
     {
       path: '/about',
